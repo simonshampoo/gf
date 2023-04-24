e78bbd84465c5593aa2fead3945cb86526656ef1
Simon Shamoon <simonshm5@gmail.com>
2023-04-24 09:35:11 -0500

    ok

--- mempool/mempool.go
+++ mempool/mempool.go
@@ -5,27 +5,27 @@ import (
 	"fmt"
 
 	"github.com/ethereum/go-ethereum/common"
-
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient/gethclient"
 )
 
 func RetrievePendingTx(client *gethclient.Client) {
 
 	ctx := context.Background()
-	txHashes := make(chan common.Hash)
-	sub, err := client.SubscribePendingTransactions(ctx, txHashes)
+	txs := make(chan *types.Transaction)
+	sub, err := client.SubscribeFullPendingTransactions(ctx, txs)
 
 	if err != nil {
 		panic(err)
 	}
-
+	print(sub)
 	defer sub.Unsubscribe()
 
 	for {
 		select {
-		case txHash := <-txHashes:
+		case tx := <-txs:
 			// check if transaction is profitable
-			fmt.Println(txHash)
+			fmt.Println(common.Bytes2Hex(tx.Data()))
 		case <-ctx.Done():
 			return
 		}
---
+++ transactions/transactions.go
@@ -0,0 +1,12 @@
+package transactions
+
+func ForkChain() int {
+
+	// from transaction hash, obtain state root of most recent block
+	//
+	return -1
+}
+
+func SendTransaction(transaction <-chan int) {
+
+}
